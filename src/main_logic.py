a386266b98b28555ccea49fb6b68e507d3e62b42
Bruno Lage <sonbruno1@gmail.com>
2025-09-28 11:55:32 -0300

    Delete src/main_logic.py

--- src/main_logic.py
+++
@@ -1,5 +0,0 @@
-"""Com o input, calcula o output correspondente.
-
-Precisamos decidir: se vai ser implementado normalmente, depois com IA, ou se vai direto para o modelo de Regressão Linear.
-
-"""
